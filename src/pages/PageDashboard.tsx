aa28f0e87a9977c67659a2a0c505b5d4aad64bc6
Ameil <ameilburke115@gmail.com>
2024-06-06 11:17:49 +1200

    fix updating todopage bug where if they were created on the same day the would swap places in array when updated

--- src/pages/PageDashboard.tsx
+++ src/pages/PageDashboard.tsx
@@ -32,8 +32,17 @@ const PageDashboard = ({ accountInfo }: { accountInfo: Account }) => {
   const sortByDate = (todoPages: TodoPage[]) => {
     const todoPagesWithDates = todoPages.map((todoPage) => {
       if (todoPage.todoPage_createdDate) {
-        const dateSplit = todoPage.todoPage_createdDate.split('-');
-        const todoPageDateObject = new Date(Number(dateSplit[2]), Number(dateSplit[1]) - 1, Number(dateSplit[0]));
+        const dateTimeSplit = todoPage.todoPage_createdDate.split(',');
+        // console.log(dateTimeSplit);
+
+        const dateSplit = dateTimeSplit[0].split('/');
+        const timeSplit = dateTimeSplit[1].split('-');
+
+        // console.log(dateSplit);
+        // console.log(timeSplit);
+
+        const todoPageDateObject = new Date(Number(dateSplit[2]), Number(dateSplit[1]) - 1, Number(dateSplit[0]), Number(timeSplit[0]), Number(timeSplit[1]), Number(timeSplit[2]));
+        // console.log(todoPageDateObject);
 
         let todoPageUpdated: any = todoPage;
         todoPageUpdated.todoPage_createdDate = todoPageDateObject;
@@ -43,6 +52,21 @@ const PageDashboard = ({ accountInfo }: { accountInfo: Account }) => {
 
     const sortedDates = todoPagesWithDates.sort((a: any, b: any) => { return a.todoPage_createdDate.getTime() - b.todoPage_createdDate.getTime() });
     setAllTodoPagesFromApi(sortedDates);
+
+
+    // const todoPagesWithDates = todoPages.map((todoPage) => {
+    //   if (todoPage.todoPage_createdDate) {
+    //     const dateSplit = todoPage.todoPage_createdDate.split('-');
+    //     const todoPageDateObject = new Date(Number(dateSplit[2]), Number(dateSplit[1]) - 1, Number(dateSplit[0]));
+
+    //     let todoPageUpdated: any = todoPage;
+    //     todoPageUpdated.todoPage_createdDate = todoPageDateObject;
+    //     return todoPageUpdated;
+    //   }
+    // });
+
+    // const sortedDates = todoPagesWithDates.sort((a: any, b: any) => { return a.todoPage_createdDate.getTime() - b.todoPage_createdDate.getTime() });
+    // setAllTodoPagesFromApi(sortedDates);
   }
 
   const getTodosForActivePageFromApi = async (givenPageId?: number) => {
@@ -203,158 +227,3 @@ const PageDashboard = ({ accountInfo }: { accountInfo: Account }) => {
 }
 
 export default PageDashboard
-
-
-// const [intialPageLoad, setIntialPageLoad] = useState<boolean>(true);
-// const [allPagesArray, setAllPagesArray] = useState<TodoPage[]>([]);
-// const [allPagesTodoArray, setAllPagesTodoArray] = useState<Todo[]>([]);
-// const [activePageId, setActivePageId] = useState<number>(-1);
-// const [activePageInfo, setActivePageInfo] = useState<TodoPage | undefined>(undefined);
-// const [activePageTodoArray, setActivePageTodoArray] = useState<Todo[]>([]);
-
-// // re-write this to make it more clearer now that backend has been implemented.
-
-// // start with getting pages, implementing empty array checks
-// // creating new pages
-// // deleting pages
-// // editing pages
-
-// const getTodoArrayForActivePage = async (pageId: number) => {
-//   if (typeof accountInfo !== "undefined") {
-//     const todoArray = await GetAllTodosForAPage(pageId);
-//     // console.log(todoArray);
-
-//     if (typeof todoArray !== 'string') {
-//       setActivePageTodoArray(todoArray);
-//     }
-//   }
-// }
-
-// const getAllTodoPages = async () => {
-//   const apiResponse = await GetAllTodoPages(accountInfo.account_id);
-//   console.log(apiResponse);
-
-//   if (typeof apiResponse !== 'string') {
-//     setAllPagesArray(apiResponse[0]);
-//     setAllPagesTodoArray(apiResponse[1]);
-//     setIntialPageLoad(false);
-//   }
-// }
-
-// const setDefaultActivePage = async () => {
-//   console.log(`setting default page`);
-
-//   if (allPagesArray.length > 0) {
-//     setActivePageId(allPagesArray[0].todoPage_id);
-//     setActivePageInfo(allPagesArray[0]);
-//   } else {
-//     setActivePageId(-1);
-//   }
-
-
-//   const apiResponse = await GetAllTodosForAPage(allPagesArray[0].todoPage_id);
-//   if (typeof apiResponse != 'string') {
-//     // console.log(apiResponse);
-//     setActivePageTodoArray(apiResponse);
-//   }
-// }
-
-// const getActivePageInfo = () => {
-//   const activeTodoPageInfo = allPagesArray.filter((todoPage: TodoPage) => {
-//     if (todoPage.todoPage_id === activePageId) {
-//       return todoPage;
-//     }
-//   });
-//   setActivePageInfo(activeTodoPageInfo[0]);
-//   // console.log(activeTodoPageInfo);
-// }
-
-// const createNewTodoPage = async () => {
-//   const newTodo = await apiRequestCreateNewTodoPage(accountInfo.account_id);
-
-//   if (typeof newTodo !== 'string') {
-//     setActivePageId(newTodo.todoPage_id);
-//     getActivePageInfo();
-//     await getAllTodoPages();
-//     await getTodoArrayForActivePage(newTodo.todoPage_id);
-//   }
-// }
-
-// const deleteTodoPage = async () => {
-//   const test = await apiRequestDeleteIndividualTodoPage(activePageId);
-
-//   if (typeof test !== 'string') {
-//     await getAllTodoPages().then(async () => {
-//       await setDefaultActivePage();
-//       setActivePageId(-1);
-//     });
-//   }
-
-// }
-
-// const changeActivePage = async (pageId: number) => {
-
-//   if (pageId !== activePageId) {
-//     setActivePageId(pageId);
-//     getTodoArrayForActivePage(pageId);
-//     getActivePageInfo();
-//   }
-// }
-
-// useEffect(() => {
-
-//   if (intialPageLoad) {
-//     getAllTodoPages();
-//   }
-
-//   if (allPagesArray.length !== 0 && activePageId === -1) {
-//     setDefaultActivePage();
-//   }
-
-//   // getActivePageInfo();
-//   console.log(activePageInfo);
-
-// }, [allPagesArray, activePageId, activePageInfo, activePageTodoArray]);
-
-
-// return (
-//   <HStack justifyContent="space-evenly" spacing="0">
-//     <VStack>
-//       <VStack justifyContent="space-between">
-//         {
-//           allPagesArray.map((todoPage) => {
-//             return <Button key={todoPage.todoPage_id} onClick={() => { changeActivePage(todoPage.todoPage_id) }}>{todoPage.todoPage_heading}</Button>
-//           })
-//         }
-//         <Button onClick={createNewTodoPage}>Create new page</Button>
-//         {
-//           activePageInfo !== undefined
-//             ? <ComponentSettingsMenu deleteTodoPage={deleteTodoPage} />
-//             : <></>
-//         }
-//       </VStack>
-//     </VStack>
-//     <VStack>
-//       <HStack></HStack>
-//       <VStack alignItems="flex-start">
-
-//         {
-//           activePageInfo !== undefined
-//             ? <Text>{activePageInfo.todoPage_heading}</Text>
-//             : <></>
-//         }
-
-//         {
-//           activePageTodoArray.map((todo: Todo) => {
-//             return <ComponentTodoPageContent key={todo.todo_id} todo={todo} activePageTodoArray={activePageTodoArray} setActivePageTodoArray={setActivePageTodoArray} />
-//           })
-//         }
-//         {
-//           activePageId === -1
-//             ? <Text>create a new page using the create new page button</Text>
-//             : <ComponentAddNewItemInput activePageTodoArray={activePageTodoArray} setActivePageTodoArray={setActivePageTodoArray} activePageId={activePageId} />
-//         }
-//       </VStack>
-//     </VStack>
-//   </HStack >
-// )
